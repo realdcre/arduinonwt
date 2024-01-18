39fce91c7626c80281ba30e70bdff5db72889912
dcre <107809443+realdcre@users.noreply.github.com>
2024-01-18 12:59:25 +0100

    fixed missing ;

--- read/read_from_sensor4w-delay/read_from_sensor4w-delay.ino
+++ read/read_from_sensor4w-delay/read_from_sensor4w-delay.ino
@@ -12,7 +12,7 @@ void loop() {
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
   Serial.println(voltage);
-  delay(500)
+  delay(500);
 }
 
 // sign:arduinonwt/realdcre
